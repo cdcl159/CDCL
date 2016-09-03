0e7a6235e9d1f403195a5d55c087621dfd30eb9d
cdcl159 <cdclwebmaster@gmail.com>
2016-09-03 10:31:18 +0100

    added announcement support to index page

--- cdclSite/static/js/announcements.js
+++ cdclSite/static/js/announcements.js
@@ -21,21 +21,12 @@ function submitAnnouncementForm() {
 
     $("#submit").click(function() {
 
-        console.log($("#title").val());
-        console.log($("#body").val());
-        console.log($(".postToDropdown").val());
-
         $("#id_title").val($("#title").val());
         $("#id_body").val($("#body").val());
         $("#id_postTo").val($(".postToDropdown").val());
-
-        console.log($("#id_title").val());
-        console.log($("#id_body").val());
-        console.log($("#id_postTo").val());
 
         $("#announcementForm").submit();
 
     });
   
-
 }
