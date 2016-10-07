6cc01d121ea37e677f29c16ef844b9a7fdf9f94d
cdcl159 <cdclwebmaster@gmail.com>
2016-10-07 20:20:00 +0100

    changed int fields to float for results submission form

--- cdclSite/static/js/resultsSubmission.js
+++ cdclSite/static/js/resultsSubmission.js
@@ -117,50 +117,19 @@ function submitResults() {
         $("#id_board5_awayPlayerID").val($(".playerDropdown_b5_a").val());
         $("#id_board6_awayPlayerID").val($(".playerDropdown_b6_a").val());
 
-        $("#id_board1_homePlayerScore").val($("#board_1_homeScore").val());
-        $("#id_board2_homePlayerScore").val($("#board_2_homeScore").val());
-        $("#id_board3_homePlayerScore").val($("#board_3_homeScore").val());
-        $("#id_board4_homePlayerScore").val($("#board_4_homeScore").val());
-        $("#id_board5_homePlayerScore").val($("#board_5_homeScore").val());
-        $("#id_board6_homePlayerScore").val($("#board_6_homeScore").val());
+        $("#id_board1_homePlayerScore").val(parseFloat($("#board_1_homeScore").val()));
+        $("#id_board2_homePlayerScore").val(parseFloat($("#board_2_homeScore").val()));
+        $("#id_board3_homePlayerScore").val(parseFloat($("#board_3_homeScore").val()));
+        $("#id_board4_homePlayerScore").val(parseFloat($("#board_4_homeScore").val()));
+        $("#id_board5_homePlayerScore").val(parseFloat($("#board_5_homeScore").val()));
+        $("#id_board6_homePlayerScore").val(parseFloat($("#board_6_homeScore").val()));
 
-        $("#id_board1_awayPlayerScore").val($("#board_1_awayScore").val());
-        $("#id_board2_awayPlayerScore").val($("#board_2_awayScore").val());
-        $("#id_board3_awayPlayerScore").val($("#board_3_awayScore").val());
-        $("#id_board4_awayPlayerScore").val($("#board_4_awayScore").val());
-        $("#id_board5_awayPlayerScore").val($("#board_5_awayScore").val());
-        $("#id_board6_awayPlayerScore").val($("#board_6_awayScore").val());
-
-
-        alert("fixture id", $("#id_selectedFixtureID").val());
-        alert("home team", $("#id_homeTeam").val());
-        alert("away team", $("#id_awayTeam").val());
-
-        alert("board1_h_playerid", $("#id_board1_homePlayerID").val());
-        alert("board2_h_playerid", $("#id_board2_homePlayerID").val());
-        alert("board3_h_playerid", $("#id_board3_homePlayerID").val());
-        alert("board4_h_playerid", $("#id_board4_homePlayerID").val());
-        alert("board5_h_playerid", $("#id_board5_homePlayerID").val());
-        alert("board6_h_playerid", $("#id_board6_homePlayerID").val());
-        alert("board1_a_playerid", $("#id_board1_awayPlayerID").val());
-        alert("board2_a_playerid", $("#id_board2_awayPlayerID").val());
-        alert("board3_a_playerid", $("#id_board3_awayPlayerID").val());
-        alert("board4_a_playerid", $("#id_board4_awayPlayerID").val());
-        alert("board5_a_playerid", $("#id_board5_awayPlayerID").val());
-        alert("board6_a_playerid", $("#id_board6_awayPlayerID").val());
-        alert("board1_h_playerscore", $("#id_board1_homePlayerScore").val());
-        alert("board2_h_playerscore", $("#id_board2_homePlayerScore").val());
-        alert("board3_h_playerscore", $("#id_board3_homePlayerScore").val());
-        alert("board4_h_playerscore", $("#id_board4_homePlayerScore").val());
-        alert("board5_h_playerscore", $("#id_board5_homePlayerScore").val());
-        alert("board6_h_playerscore", $("#id_board6_homePlayerScore").val());
-        alert("board1_a_playerscore", $("#id_board1_awayPlayerScore").val());
-        alert("board2_a_playerscore", $("#id_board2_awayPlayerScore").val());
-        alert("board3_a_playerscore", $("#id_board3_awayPlayerScore").val());
-        alert("board4_a_playerscore", $("#id_board4_awayPlayerScore").val());
-        alert("board5_a_playerscore", $("#id_board5_awayPlayerScore").val());
-        alert("board6_a_playerscore", $("#id_board6_awayPlayerScore").val());
-
+        $("#id_board1_awayPlayerScore").val(parseFloat($("#board_1_awayScore").val()));
+        $("#id_board2_awayPlayerScore").val(parseFloat($("#board_2_awayScore").val()));
+        $("#id_board3_awayPlayerScore").val(parseFloat($("#board_3_awayScore").val()));
+        $("#id_board4_awayPlayerScore").val(parseFloat($("#board_4_awayScore").val()));
+        $("#id_board5_awayPlayerScore").val(parseFloat($("#board_5_awayScore").val()));
+        $("#id_board6_awayPlayerScore").val(parseFloat($("#board_6_awayScore").val()));
 
         $("#resultsForm").submit();
 
