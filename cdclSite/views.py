8122a7b1ca10108b496faf154e21b8560ed1889e
cdcl159 <cdclwebmaster@gmail.com>
2016-11-10 19:33:27 +0000

    order events by name

--- cdclSite/views.py
+++ cdclSite/views.py
@@ -127,7 +127,7 @@ def resultsPage(request):
 
 
 
-	return render(request, "cdclSite/resultsPage.html", {"data": json.dumps(resultsData), "events": Event.objects.all()})
+	return render(request, "cdclSite/resultsPage.html", {"data": json.dumps(resultsData), "events": Event.objects.all().orderBy("name")})
 
 
 def registrationPage(request):
