c16d89e2ad9a21da60c82d21ed39a48e5fa10579
cdcl159 <cdclwebmaster@gmail.com>
2016-11-10 20:07:31 +0000

    added sorting to js

--- cdclSite/static/js/resultsPage.js
+++ cdclSite/static/js/resultsPage.js
@@ -5,6 +5,24 @@ function populateTable(eventName, data) {
 
 	var dataForEvent = data[eventName];
 
-	
+	var orderable = [];
+
+	for (team in dataForEvent["teamData"]) {
+		orderable.push(dataForEvent["teamData"][team]);
+	}
+
+	orderable.sort(function(a, b) {
+
+		return a["score"] - b["score"];
+
+	});
+
+	orderable.sort(function(a, b) {
+		if (a["score"] == b["score"]) {
+			return a["boardsFor"] - b["boardsFor"];
+		}
+	});
+
+	console.log(orderable);
 
 }
