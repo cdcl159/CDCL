5a562e65d6c3131db6ab698f4812bca7ad270251
cdcl159 <cdclwebmaster@gmail.com>
2017-07-05 19:41:54 +0100

    reporter

---
+++ reporter.py
@@ -0,0 +1,43 @@
+from cdclSite.models import *
+
+
+def report_all(output_filename):
+	output = []
+	for e in Event.objects.all():
+		
+		for fixture in e.fixture_set.all():
+			fixture_data = {}
+			fixture_data["hometeam"] = fixture.homeTeam.name
+			fixture_data["awayteam"] = fixture.awayTeam.name
+			fixture_data["date"] = str(fixture.date)
+
+			games = []
+			for game in fixture.homeSubmission.game_set.all():
+				homeplayer = Player.objects.get(id = game.homePlayerID)
+				awayplayer = Player.objects.get(id = game.awayPlayerID)
+				games.append(
+					{
+						"boardnumber": game.boardNumber,
+						"homeplayer": {
+							"id": game.homePlayerID,
+							"name": homeplayer.forenames + " " + homeplayer.surname,
+							"ecf": homeplayer.ecfCode,
+							"score": game.homePlayerScore
+						},
+						"awayplayer": {
+							"id": game.awayPlayerID,
+							"name": awayplayer.forenames + " " + awayplayer.surname,
+							"ecf": awayplayer.ecfCode,
+							"score": game.awayPlayerScore
+						},
+
+						
+					}
+				)
+			
+			fixture_data["games"] = games
+				
+		
+		output.append(fixture_data)
+
+	return output
